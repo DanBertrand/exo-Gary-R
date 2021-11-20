412e80965adfa1c3a6671b6351c4376cf5fdaa21
DanBertrand <dan.bertrand.hs@gmail.com>
2021-11-20 18:44:14 +0100

    Add few comments

--- exoGary.R
+++ exoGary.R
@@ -1,13 +1,14 @@
 names <- list("Red [Franz]", "Gr(een)", "Gary")
 nicknames     <- "Ade/i, Do(i)lfal+Do(i)lferl"
 
-# Function take a list and 
-remove_pattern_in_list <- function(list, regex) {
+# Function take a list and a pattern to apply to all list element
+remove_pattern_in_list <- function(list, pattern) {
+  
   return_list <- list()
   i=1
 
   while(i< (length(list) + 1) ) {
-     return_list[[i]]  <- gsub(regex, '', list[i])
+     return_list[[i]]  <- gsub(pattern, '', list[i])
     i <- i + 1
   }
   return(return_list)
@@ -16,9 +17,10 @@ remove_pattern_in_list <- function(list, regex) {
 # pattern to remove parenthesis () and word inside => gsub("\\s*\\([^\\)]+\\", '')
 # pattern to remove brackets [] and word inside => gsub('\\[.*?\\]', '', word)
 
-# Remove all Names wich are into brackets []
+# Remove all Names in a given list wich are into brackets []
 remove_pattern_in_list(names, '\\[.*?\\]')
-#  Output : 
+# Input: list("Red [Franz]", "Gr(een)", "Gary")
+#  Output: 
   # [[1]]
   # [1] "Red "
 
@@ -29,13 +31,13 @@ remove_pattern_in_list(names, '\\[.*?\\]')
   # [1] "Gary"
 
 
-# Function qui prend en argument une string, un caractere qu'on veut changer, le caracter de remplacement
+# Function qui prend en argument une string, un caractere qu'on veut changer, un caractere de remplacement
 change_caractere_in_string <- function(string, caractere_to_change, replacement_caractere) {
 
   # On met le caractere dans le pattern (Regex) 
   pattern <- sprintf('[%s\\]', caractere_to_change)
 
-  # On retourne la 
+  # On retourne la string modifié
   return(gsub(pattern, replacement_caractere, string))
 }
 
